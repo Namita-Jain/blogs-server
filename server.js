f8bdb4f3adcf9bab283d12d250b8ba8a86874b89
Namita Jain <namita.jain@joshtechnologygroup.com>
2020-08-23 12:11:55 +0530

    change port

--- server.js
+++ server.js
@@ -120,5 +120,4 @@ server.use(router)
 
 const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
 server.listen(PORT, LOCAL_ADDRESS, () => {
-  console.log('server listening at', server.address());
 });
